467471b1c4386fff4adb48bd02be4f6ab2f1d0b6
llRedoctoberll <70113539+llRedoctoberll@users.noreply.github.com>
2020-09-06 19:12:49 -0500

    Added PokemonList variable
    -Removed existing script from scripts.js
    -Added new pokemonList variable with array and objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-alert('Hello world');
-
-let favoriteFood = 'Pasta Salad';
-document.write(favoriteFood);
+let pokemonList = [
+    { name: "Fearow", height: 1.2, type: ["flying", "normal"] },
+    { name: "Butterfree", height: 1.1, type: ["bug", "flying"] },
+    { name: "Gyarados", height: 6.5, type: ["water", "flying"] }
+];
+document.write(pokemonList);
