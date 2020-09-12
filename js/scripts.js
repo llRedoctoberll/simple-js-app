34fe172ace0b6fb0ca8802245868bbea87b7af19
llRedoctoberll <70113539+llRedoctoberll@users.noreply.github.com>
2020-09-12 17:07:09 -0500

    Ex 1.3 Changes
    css/styles Changes
    - Added padding to body - 50px
    -Added Width, padding, border and margin to .box
    
    Script Changes
    - added new Pokemon repository
    - added new loop for the height of the pokemons

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,43 @@
-let pokemonList = [
-    { name: "Fearow", height: 1.2, type: ["flying", "normal"] },
-    { name: "Butterfree", height: 1.1, type: ["bug", "flying"] },
-    { name: "Gyarados", height: 6.5, type: ["water", "flying"] }
+var repository = [
+    { name: "Venonat", height: 1.0, types: ["bug", "poison"] },
+    { name: "Arbok", height: 3.5, types: ["poison"] },
+    { name: "Golbat", height: 1.6, types: ["flying", "poison"] },
+    { name: "Diglett", height: 0.2, types: ["ground"] },
 ];
-document.write(pokemonList);
+
+for (var item = 0; item < repository.length; item++) {
+    var size;
+    if (repository[item].height > 1) {
+        size = "Large Pokemon";
+    } else {
+        size = "Small Pokemon";
+    }
+
+    var result;
+    for (var typeItem = 0; typeItem < repository[item].types.length; typeItem++) {
+        if (repository[item].types[typeItem] == "grass") {
+            result = '<span style="color:green;"> ';
+        } else if (repository[item].types[typeItem] == "fire") {
+            result = '<span style="color:red;"> ';
+        } else if (repository[item].types[typeItem] == "psychic") {
+            result = '<span style="color:yellow;"> ';
+        } else if (repository[item].types[typeItem] == "poison") {
+            result = '<span style="color:rgb(106, 42, 106);"> ';
+        }
+    }
+    document.write(
+        '<div class="box">' +
+        repository[item].name +
+        "(height: " +
+        repository[item].height +
+        "m" +
+        ")" +
+        "<br>" +
+        size +
+        result +
+        "<br>" +
+        repository[item].types +
+        "<br>" +
+        "</div>"
+    );
+}
